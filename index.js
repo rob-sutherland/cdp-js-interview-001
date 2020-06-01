ef6182b99fbeed9213b6e6863611d0db3d50a286
rob-sutherland <rob.sutherland@cdpemail.dhs.gov>
2020-06-01 10:36:53 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 
-const App = () => {
+const App = () => { 
   return (
     <div>
       Hello! Write the code to complete this assignment in the index.js file.
