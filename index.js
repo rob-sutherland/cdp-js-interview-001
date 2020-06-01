cbc690aa7fd0bc8d8430728d9285bdf9f541150a
rob-sutherland <rob.sutherland@cdpemail.dhs.gov>
2020-06-01 17:05:24 -0500

    removed random number function

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 
-// returns an random interger in the interval [min, max]
-const randomNumberInRange = function(min, max) {
-  return Math.floor(Math.random() * Math.floor(max - min + 1)) + min;
-}
-
 const App = () => { 
   return (
     <div>
