d83506d558c756aea084a6459d63e1ef3175cc0f
rob-sutherland <rob.sutherland@cdpemail.dhs.gov>
2020-06-01 09:41:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cdp-js-interview-001
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/cdp-js-interview-001)
---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      Hello! Write the code to complete this assignment in the index.js file.
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
